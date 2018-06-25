6be8f6b5a4e53901bea37ad9ef2554f8737929b8
xuhao <1965693034@qq.com>
2018-06-25 19:11:58 +0800

    登陆模块完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -3,6 +3,11 @@
  */
 $(function() {
   $('#form').bootstrapValidator({
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
     fields: {
       username: {
         validators: {
@@ -13,6 +18,9 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须在 2-6位"
+          },
+          callback: {
+            message: "用户名不存在"
           }
         }
       },
@@ -25,9 +33,37 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须在 6-12位"
+          },
+          callback: {
+            message: "密码错误"
           }
         }
       }
     }
+  });
+
+  $('#form').on('success.form.bv', function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/employee/employeeLogin',
+      data: $('#form').serialize(),
+      dataType: 'json',
+      success: function (info) {
+        if(info.success) {
+          location.href = "index.html"
+        }
+        if(info.error === 1000) {
+          $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback')
+        }
+        if(info.error === 1001) {
+          $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback')
+        }
+      }
+    })
+  })
+
+  $('[type="reset"]').click(function() {
+    $('#form').data('bootstrapValidator').resetForm()
   })
 })
