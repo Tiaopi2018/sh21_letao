00b2c03566510c96e3b849827ffecb376813832a
xuhao <1965693034@qq.com>
2018-06-25 12:50:56 +0800

    登陆模块初步完成

---
+++ public/back/js/login.js
@@ -0,0 +1,33 @@
+/**
+ * Created by xuhao on 2018/6/25.
+ */
+$(function() {
+  $('#form').bootstrapValidator({
+    fields: {
+      username: {
+        validators: {
+          notEmpty: {
+            message: "用户名不能为空"
+          },
+          stringLength: {
+            min: 2,
+            max: 6,
+            message: "用户名长度必须在 2-6位"
+          }
+        }
+      },
+      password: {
+        validators: {
+          notEmpty: {
+            message: "密码不能为空"
+          },
+          stringLength: {
+            min: 6,
+            max: 12,
+            message: "密码长度必须在 6-12位"
+          }
+        }
+      }
+    }
+  })
+})
